10faba9a2980c76acc6f0540491701267032df93
David Santos <davidjaneiro92@gmail.com>
2021-03-20 16:34:31 -0300

    Projeto criado

---
+++ cabecalho.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, 
+	shrink-to-fit=no"> <!--para configurar a pagina abri no celular -->
+	<meta name="description" content="Portal de Cursos da Escola Q-Cursos
+	networks, Participe das nossa formaçãoes  e seja  um profissional 
+	reconhacido"> <!-- para os buscador achar a sua pagina-->
+	<meta name="author" content="Hugo Vasconcelos">
+	<meta name="keywords" content="Cursos, portal de cursos, cursos de cecnologia, cusos de programação "> <!--palavras chaves -->
+	<title>Portal Q-Cursos</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+	<link rel="stylesheet" href="css/estilos-site101223.css"> 
+	<link rel="stylesheet" href="css/estilos-padrao.css"> 
+	<link rel="stylesheet" href="css/cursos.css"> 
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+
+	<script src="js/validation.js"></script>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	
+</head>
+
+<body id="page-top">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container">
+		<a class="navbar-brand js-scroll-trigger" href="index.php#page-top">
+		<img src="img/logo-qcursos.png" class="img-logo"> <span class="texto-logo">Portal Q-Cursos</a>
+		<button class="navbar-toggler navbar-toggler-right" type="button"
+		data-toggle="collapse" data-target="#navbarResponsive " 
+		arial-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle Navigation">
+			Menu <i class="fas fa-bars"></i>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav text-uppercase ml-auto texto_menu">
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="index.php#Formacoes">Formações</a>
+				</li>
+					<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="index.php#qcursos">Cursos</a>
+				</li>
+					<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="index.php#cursos">Q-Cursos</a>
+				</li>
+					<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="index.php#alunos">Alunos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="index.php#contatos">Contatos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="login.php" target="_blank">Login</a>
+					<!--target="_blank" abri outra aba-->
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+		      <button class="btn btn-outline-light my-2 my-sm-0 botao-buscar" type="submit"><i class="fas fa-search"></i></button>
+
+
+			</div>
+		</div>
+
+
+	</nav>
---
+++ login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<title>Login - Portal Q-Cursos</title>
+	<meta charset="utf-8">
+
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+	<link rel="stylesheet" href="css/estilosLogin10.css"> 
+	
+	
+</head>
+<body>
+
+	<div class="container h-100">
+
+		<div class="d-flex justify-content-center h-100">
+
+			<div class="card_login">
+
+				<div class="d-flex justify-content-center">
+
+					<div class="area_logo">
+
+						<a href="index.php"><img src="img/logo.png" class="logo_circular"></a>
+
+					</div>
+
+				</div>
+
+				<div class="d-flex justify-content-center form_login">
+
+					<form>
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="far fa-user"></i></span>
+							</div>
+							<input type="text" name="" class="form-control input_usuario" placeholder="Usuário">
+						</div>
+
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-lock"></i></i></i></span>
+							</div>
+							<input type="password" name="" class="form-control input_usuario" placeholder="Senha">
+						</div>
+
+						<div class="input-group">
+							<div class="custom-control custom-checkbox">
+								<input type="checkbox" name="" class="custom-control-input" id="customControlInline">
+								<label class="custom-control-label" for="customControlInline">Lembrar-me</label>
+							</div>
+							
+						</div>
+
+
+					</form>
+
+				</div>
+
+				<div class="d+d-flex justify-content-center mt-3 area_botao">
+					<button type="button" name="button" class="btn btn_login">Login</button>
+					
+				</div>
+				<div class="mt-4 area_links">
+					<div class="d-flex justify-content-center link">
+						Não possui cadastro?<a href="#" class="ml-2">Cadastra-se<a>
+						
+					</div>
+					<div class="d-flex justify-content-center link">
+						<a href="#" class="ml-2">Recuperar Senha <a>
+						
+					</div>
+					
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
